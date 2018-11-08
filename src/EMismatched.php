1fb7e26b69ba0b09648b3ac95ac4c7bf2a67893c
hacpaka <hacpaka@eggbe.com>
2018-11-08 07:27:10 +0300

    +

---
+++ src/EMismatched.php
@@ -0,0 +1,25 @@
+<?php
+namespace Able\Exceptions;
+
+use \Able\Exceptions\Abstractions\AException;
+use \Able\Helpers\Arr;
+
+use \Throwable;
+
+class EMismatched extends AException {
+
+	/**
+	 * @var string
+	 */
+	protected static string $template = "The value is mismatched: %s expected but %s given!";
+
+	/**
+	 * @param string $expected
+	 * @param string $given
+	 *
+	 * @param Throwable|null $Previous
+	 */
+	public function __construct(string $expected, string $given, ?Throwable $Previous = null) {
+		parent::__construct(func_get_args());
+	}
+}
